c9be38bb9477d73a484bdd45121edae301194c55
redassser <redryan890@gmail.com>
2020-11-21 13:50:44 -0500

    switcheroo

--- events/message.js
+++ events/message.js
@@ -8,7 +8,7 @@ module.exports = (client, message) => {
     ];
     if(possCommands.includes(message.content.toLowerCase())) {
         if (!message.member.permissionsIn(message.channel).has("ADMINISTRATOR")) {message.channel.send("Sorry! Admins only");return;}
-        if(!message.guild.members.cache.get(client.user.id).hasPermission("MANAGE_CHANNELS")) {message.channel.send("Sorry! I don't have the ``manage channels`` permission in "+message.guild.name+"!");return;}
+        if(!message.guild.members.cache.get(client.user.id).hasPermission("MANAGE_CHANNELS")) {message.channel.send("Sorry! I don't have the ``manage channels`` permission in this server!");return;}
         if (message.guild.channels.cache.find(h=>h.type==="category"&&h.name==="auto-vc")) {message.channel.send("You already have an `auto-vc`!");return;}
         message.guild.channels.create("auto-vc",
             {
